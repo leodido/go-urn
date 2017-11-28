7ae36b32656d1b69a6ce3a692158fad4f6348adf
Leonardo Di Donato <l.didonato@fabbricadigitale.it>
2017-11-28 16:34:40 +0100

    New: URN public API

--- urn.go
+++ urn.go
@@ -1,7 +1,97 @@
 package urn
 
-// Urn is ...
-type Urn struct {
-	ID string
-	SS string
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/leodido/go-urn/grammar"
+)
+
+// URN represents an Uniform Resource Name.
+//
+// The general form represented is:
+//
+//	urn:<id>:<ss>
+//
+// Details at https://tools.ietf.org/html/rfc2141
+type URN struct {
+	ID   string // Namespace identifier
+	SS   string // Namespace specific string
+	tree string
+}
+
+// Error describes an error and the input that caused it.
+type Error struct {
+	Source string
+	Column int
+	Detail string
+}
+
+func (e *Error) Error() string {
+	return "\"" + e.Source + "\" causes syntax error at character " + strconv.Itoa(e.Column) + ", " + e.Detail
+}
+
+type errorListener struct {
+	*antlr.DefaultErrorListener
+}
+
+func (l *errorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	is := e.GetOffendingToken().GetInputStream()
+
+	panic(&Error{
+		Source: fmt.Sprintf("%s", is),
+		Column: column,
+		Detail: msg,
+	})
+}
+
+// Parse is ...
+func Parse(u string) (*URN, error) {
+	urn, err := parse(u)
+	if err != nil {
+		return nil, err
+	}
+
+	return urn, err
+}
+
+func parse(u string) (urn *URN, err error) {
+	stream := antlr.NewInputStream(u)
+	lexer := grammar.NewUrnLexer(stream)
+	tokens := antlr.NewCommonTokenStream(lexer, 0)
+	parser := grammar.NewUrnParser(tokens)
+	parser.RemoveErrorListeners()
+	parser.AddErrorListener(new(errorListener))
+
+	defer func() {
+		if r := recover(); r != nil {
+			urn = nil
+			err = r.(error)
+		}
+	}()
+
+	urn = &URN{}
+	parser.AddParseListener(NewListener(urn))
+	parser.Urn()
+
+	return
+}
+
+// String reassembles the URN into a valid URN string.
+//
+// This requires both ID and SS fields to be non-empty.
+// Otherwise it returns an empty string.
+func (u *URN) String() string {
+	var res string
+	if u.ID != "" && u.SS != "" {
+		res = "urn:" + u.ID + ":" + u.SS
+	}
+
+	return res
+}
+
+// Tree returns a string representation of the resulting concrete syntax tree.
+func (u *URN) Tree() string {
+	return u.tree
 }
