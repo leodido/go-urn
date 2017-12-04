4a723a6fd23c8567c3ab6de0a0a485d71de6534a
Leonardo Di Donato <l.didonato@fabbricadigitale.it>
2017-12-04 12:28:03 +0100

    Update: Preparing listener test for testing tables

--- listener_test.go
+++ listener_test.go
@@ -1,38 +1,41 @@
 package urn
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/leodido/go-urn/grammar"
 )
 
-func TestX(t *testing.T) {
-	inFile, _ := os.Open("testdata/00.urn")
-	defer inFile.Close()
-	scanner := bufio.NewScanner(inFile)
-	scanner.Split(bufio.ScanLines)
+func TestListener(t *testing.T) {
 
-	for scanner.Scan() {
-		input := scanner.Text()
+	inputs := []string{
+	//"URN:1ab:xxx",
+	}
 
+	fmt.Println("---")
+	for _, input := range inputs {
 		stream := antlr.NewInputStream(input)
 		lexer := grammar.NewUrnLexer(stream)
 		tokens := antlr.NewCommonTokenStream(lexer, 0)
 
 		parser := grammar.NewUrnParser(tokens)
-		// parser.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+		parser.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+		parser.BuildParseTrees = true
 
-		urn := Urn{}
-		parser.AddParseListener(NewListener(&urn))
-
-		// parser.BuildParseTrees = true
+		/*
+			urn := URN{}
+			parser.AddParseListener(NewListener(&urn))
+		*/
 
 		parser.Urn()
 
-		fmt.Printf("%+v\n", urn)
+		//fmt.Printf("getstruct=> %+v\n", tree.GetStructure())
+		//fmt.Println(tree.ToStringTree(nil, parser))
+
+		// fmt.Printf("%s => %+v\n", input, urn)
+
+		fmt.Println("---")
 	}
 }
