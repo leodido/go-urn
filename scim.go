fdfa6390ee46e846db54a24d192bc47f3a628609
Leonardo Di Donato <leodidonato@gmail.com>
2024-01-26 14:53:54 +0100

    refactor: move SCIM schema types to their own module
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- scim.go
+++ scim.go
@@ -1,42 +1,9 @@
 package urn
 
-type SCIMType int
-
-const (
-	Unsupported SCIMType = iota
-	Schemas
-	API
-	Param
-)
-
-func (t SCIMType) String() string {
-	switch t {
-	case Schemas:
-		return "schemas"
-	case API:
-		return "api"
-	case Param:
-		return "param"
-	}
-
-	return ""
-}
-
-func SCIMTypeFromString(input string) SCIMType {
-	switch input {
-	case "schemas":
-		return Schemas
-	case "api":
-		return API
-	case "param":
-		return Param
-	}
-
-	return Unsupported
-}
+import scimschema "github.com/leodido/go-urn/scim/schema"
 
 type SCIM struct {
-	Type  SCIMType
+	Type  scimschema.Type
 	Name  string
 	Other string
 	pos   int
