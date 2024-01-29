365c2aec8756479415967595dd9c3c4f6fe10017
Leonardo Di Donato <leodidonato@gmail.com>
2024-01-29 20:53:51 +0100

    test: check equality with nil gives false
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- urn_test.go
+++ urn_test.go
@@ -22,29 +22,6 @@ func TestParseSignature(t *testing.T) {
 	assert.False(t, ok)
 }
 
-func TestLexicalEquivalence(t *testing.T) {
-	for ii, tt := range equivalenceTests {
-		urnlx, oklx := Parse(tt.lx)
-		urnrx, okrx := Parse(tt.rx)
-
-		if oklx && okrx {
-
-			assert.True(t, urnlx.Equal(urnlx))
-			assert.True(t, urnrx.Equal(urnrx))
-
-			if tt.eq {
-				assert.True(t, urnlx.Equal(urnrx), ierror(ii))
-				assert.True(t, urnrx.Equal(urnlx), ierror(ii))
-			} else {
-				assert.False(t, urnlx.Equal(urnrx), ierror(ii))
-				assert.False(t, urnrx.Equal(urnlx), ierror(ii))
-			}
-		} else {
-			t.Log("Something wrong in the testing table ...")
-		}
-	}
-}
-
 func TestJSONMarshaling(t *testing.T) {
 	t.Run("roundtrip", func(t *testing.T) {
 		// Marshal
